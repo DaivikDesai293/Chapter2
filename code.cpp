9c93ff8f073133730a191648037680c1f37a5f10
DaivikDesai293 <desaidaivik293@gmail.com>
2026-01-16 20:51:42 -0500

    Initial commit

---
+++ code.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Daivik \nDesai" ; // you can use endl instad of \n 
+    return 0;
+}
+
+
+
+
+
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     return 0;
+// }   this is the boilerplate code or syntext code!!
