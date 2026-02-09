ea8dba0df74ca30fd167b86403e7d9c7eb5e2e80
DaivikDesai <desaidaivik293@gmail.com>
2026-02-08 20:11:30 -0500

    New

---
+++ InputFromUser.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float radius = 'R';
+    cout <<"Radius Of Circle Would Be : " ;
+    cin>>radius;
+    
+    float PI = 3.14;
+    float area =  PI * (radius * radius);
+    cout<<" Area of circle is gonna be : "<<area<<endl;
+    return 0;
+
+}
+
+// write g++ InputFromUser.cpp && ./a.exe to run the code in terminal.
---
+++ varible.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age = 20;
+    cout<<"Daivik Desai's age is :"<<age<<endl;
+    return 0;
+}
